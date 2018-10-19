a434e8565090a0adc5f5a25e8416d39ff08b8b44
Jason Huh <jaeseok.huh.0@gmail.com>
2018-10-19 21:23:12 +0900

    add copy commands

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
-sudo apt-get install -y build-essential cmake git vim htop
+sudo apt-get install -y build-essential cmake git vim htop python-pip python3-pip build-essential
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+cp .bashrc ~
+cp .vimrc ~
+cp .tmux.conf ~
 
 
