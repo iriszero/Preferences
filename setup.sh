25725b1acadfbd9c961a370c542a2da981624a96
Jaeseok Huh <iriszero5@gmail.com>
2018-10-20 05:19:51 +0000

    install vim nox

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -y build-essential cmake git vim htop python-pip python3-pip build-essential
+sudo apt-get install -y build-essential cmake git vim htop python-pip python3-pip build-essential vim-nox
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 cp .bashrc ~
