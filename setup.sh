15aeb4aabcc7d8ce94bb469616bff19fa42b9f7d
jason-huh <47955510+jason-huh@users.noreply.github.com>
2019-03-29 10:10:03 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -y build-essential cmake git vim htop python-pip python3-pip build-essential vim-nox
+sudo apt-get install -y build-essential cmake git vim htop python-pip python3-pip build-essential vim-nox ibus-gtk ibus-gtk3
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 cp .bashrc ~
