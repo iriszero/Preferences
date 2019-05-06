68a5da153b9994e34582473fdcfabe2da17e63b8
Jaeseok Huh (Jason) <jaeseok.huh.0@gmail.com>
2019-05-06 12:19:55 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,19 @@
 sudo apt update && sudo apt upgrade -y
 
-sudo apt-get install -y build-essential cmake git vim htop python-pip python3-pip build-essential vim-nox ibus-gtk ibus-gtk3 tmux wireshark openssh-client openssh-server vsftpd
+sudo apt-get install -y build-essential cmake git vim htop python-pip python3-pip build-essential vim-nox ibus-gtk ibus-gtk3 tmux wireshark openssh-client openssh-server vsftpd atom
+
+# Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 cp .bashrc ~
 cp .vimrc ~
-cp .tmux.conf ~
+
 
 vim +PluginInstall +qall
 ~/.vim/bundle/youcompleteme/install.sh
 
+# Tmux
+cp .tmux.conf ~
+
+# Atom
+apm install --packages-file atom/package-list.txt
