41e2e1150fed1014deebbca2ba2d5d76afdfa534
Jaeseok Huh (Jason) <jaeseok.huh.0@gmail.com>
2020-03-06 15:16:47 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,6 +12,7 @@ bash ./change-ubuntu-mirror.sh -k
 apt update && apt upgrade -y
 apt-get install -y build-essential cmake git vim htop python-pip python3-pip build-essential vim-nox ibus-gtk ibus-gtk3 tmux wireshark openssh-client openssh-server virtualenv vsftpd xclip
 apt-get install -y blueman pavucontrol
+apt-get install -y aria2
 
 # Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
