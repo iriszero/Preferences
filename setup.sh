4537d53acad1a09d2e7e3ff223683ddd133df017
Jaeseok Huh (Jason) <jaeseok.huh.0@gmail.com>
2020-03-06 09:25:41 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ bash ./change-ubuntu-mirror.sh -k
 
 apt update && apt upgrade -y
 apt-get install -y build-essential cmake git vim htop python-pip python3-pip build-essential vim-nox ibus-gtk ibus-gtk3 tmux wireshark openssh-client openssh-server virtualenv vsftpd xclip
-
+apt-get install -y blueman
 
 # Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
