69f6915d60e80ec3f797b1d3fd8a78604572a7f9
Jason Huh <jason@instance-2.c.arcane-effect-192612.internal>
2020-08-11 14:27:56 +0000

    Add git identity

--- common/setup.sh
+++ common/setup.sh
@@ -14,3 +14,5 @@ cp .tmux.conf ~
 
 # Save your git crediental in memory (for 15 mins)
 git config --global credential.helper cache
+git config --global user.name "Jason Huh"
+git config --global user.email "jaeseok.huh.0@gmail.com"
