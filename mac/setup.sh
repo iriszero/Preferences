7cc7750281e08b88b43d05f7ac17f8743557b1a9
Jason Huh <jaeseok.huh.0@gmail.com>
2020-08-11 14:54:12 +0000

    Add a MacOS configuration

---
+++ mac/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install Homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Xcode
+xcode-select —-install
+
+# zsh
+brew install zsh
+chsh -s /usr/local/bin/zsh
+
+# https://www.freecodecamp.org/news/jazz-up-your-zsh-terminal-in-seven-steps-a-visual-guide-e81a8fd59a38/
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
