06548a839a8ebfe2e8cd2fb478039973efe61bf8
Jason Huh <jaeseok.huh.0@gmail.com>
2020-08-12 12:32:52 +0900

    Add powerline font on mac

--- mac/setup.sh
+++ mac/setup.sh
@@ -7,3 +7,12 @@ brew install cmake git htop vim zsh tmux
 brew install python python3 pyenv-virtualenv python-pip python3-pip
 brew install aria2
 
+# Powerline Fonts
+# clone
+git clone https://github.com/powerline/fonts.git --depth=1
+# install
+cd fonts
+./install.sh
+# clean-up a bit
+cd ..
+rm -rf fonts
