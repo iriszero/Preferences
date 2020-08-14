fabf172d1193c6434d560e5d7a9ad4b3e55200a9
Jason Huh <jaeseok.huh.0@gmail.com>
2020-08-14 03:48:21 +0000

    Add localhost name

--- common/post/edit_hosts.sh
+++ common/post/edit_hosts.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 BASE_DIR=$(dirname "$0")
+BASE_FILE_NAME="stevenblack"
 
+echo $(hostname -I | cut -d\  -f1) $(hostname) | sudo tee /etc/hosts > /dev/null
+cat $BASE_DIR/hosts_list/$BASE_FILE_NAME | sudo tee -a /etc/hosts > /dev/null
 for filename in $BASE_DIR/hosts_list/*; do
 	[ -e "$filename" ] || continue
-	sudo rm /etc/hosts
-	cat $filename | sudo tee /etc/hosts > /dev/null
+	
+	if [ $BASE_FILE_NAME = $filename ]; then
+		continue
+	fi
+
+	cat $filename | sudo tee -a /etc/hosts > /dev/null
 done
