a11c827543054566b3a7824457981ce1a40254d3
Jason Huh <jaeseok.huh.0@gmail.com>
2020-08-17 10:55:09 +0900

    Fix ycm's python3-dev dependency on ubuntu

--- common/post/setup.sh
+++ common/post/setup.sh
@@ -2,8 +2,24 @@
 
 BASE_DIR=$(dirname "$0")
 
+# vim
+cp $BASE_DIR/.vimrc $HOME
+
+# Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim || :
+
+cp $BASE_DIR/.vimrc $HOME
+
+vim +PluginInstall +qall
+sudo python3 $HOME/.vim/bundle/youcompleteme/install.py
+
+# tmux
+cp $BASE_DIR/.tmux.conf $HOME
+
+
 $BASE_DIR/install_ohmyzsh.sh
 $BASE_DIR/zsh_antigen_ohmyzsh.sh
+$BASE_DIR/edit_hosts.sh
 
 sudo chsh -s /usr/bin/zsh
 echo "alias ll='ls -alf'" >> $HOME/.zshrc
--- common/pre/setup.sh
+++ common/pre/setup.sh
@@ -2,20 +2,6 @@
 
 BASE_DIR=$(dirname "$0")
 
-# vim
-cp $BASE_DIR/.vimrc $HOME
-
-# Vundle
-git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim || :
-
-cp $BASE_DIR/.vimrc $HOME
-
-vim +PluginInstall +qall
-sudo python3 $HOME/.vim/bundle/youcompleteme/install.py
-
-# tmux
-cp $BASE_DIR/.tmux.conf $HOME
-
 # START git
 # Save your git crediental in memory (for 15 mins)
 git config --global credential.helper cache
