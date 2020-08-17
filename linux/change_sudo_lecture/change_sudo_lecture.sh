a975de10d55ffb93ae0f784512630d5c4178cf2a
Jason Huh <jaeseok.huh.0@gmail.com>
2020-08-17 02:08:03 +0000

    Change the ownership of the files

--- linux/change_sudo_lecture/change_sudo_lecture.sh
+++ linux/change_sudo_lecture/change_sudo_lecture.sh
@@ -1,4 +1,6 @@
 # Show the Peter Parker Principle
 sudo mv sudoers.lecture /etc/
+sudo chown root:root /etc/sudoers.lecture
 sudo mv privacy /etc/sudoers.d/
+sudo chown root:root /etc/sudoers.d/privacy
 
