7add7e0127a8d7d88258a495fc6bb1400ec601e8
Jason Huh <jaeseok.huh.0@gmail.com>
2020-08-17 11:20:56 +0900

    Add the Peter Parker Principle to zsh

--- common/post/zsh_antigen_ohmyzsh.sh
+++ common/post/zsh_antigen_ohmyzsh.sh
@@ -6,4 +6,7 @@ fi
 
 curl -L git.io/antigen > $HOME/antigen.zsh
 cat ./antigen_config_to_zshrc >> $HOME/.zshrc
-cp .antigenrc $HOME/
+\cp -f .antigenrc $HOME/
+
+# Peter Parker Principle
+echo "With great power comes great responsibility." >> $HOME/.zshrc
