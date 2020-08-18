4f46d869cec514caf0c61386c2b72074f9a38058
Jason Huh <jaeseok.huh.0@gmail.com>
2020-08-18 18:28:51 +0900

    Replace the relative path

--- common/post/zsh_antigen_ohmyzsh.sh
+++ common/post/zsh_antigen_ohmyzsh.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+BASE_DIR=$(dirname "$0")
 
 curl -L git.io/antigen > $HOME/antigen.zsh
-cat ./antigen_config_to_zshrc >> $HOME/.zshrc
-\cp -f .antigenrc $HOME/
+cat $BASE_DIR/antigen_config_to_zshrc >> $HOME/.zshrc
+\cp -f $BASE_DIR/.antigenrc $HOME/
 
 # Peter Parker Principle
 echo "echo 'With great power comes great responsibility.'" >> $HOME/.zshrc
