92487e5581061f7b939f1381c57c1e8fc11826c5
Jason Huh <jaeseok.huh.0@gmail.com>
2020-08-18 14:52:02 +0900

    Fix the Peter Parker Principle

--- common/post/zsh_antigen_ohmyzsh.sh
+++ common/post/zsh_antigen_ohmyzsh.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-if [[ $(zsh) -ne 0 ]]; then
-	exit
-fi
 
 curl -L git.io/antigen > $HOME/antigen.zsh
 cat ./antigen_config_to_zshrc >> $HOME/.zshrc
 \cp -f .antigenrc $HOME/
 
 # Peter Parker Principle
-echo "With great power comes great responsibility." >> $HOME/.zshrc
+echo "echo 'With great power comes great responsibility.'" >> $HOME/.zshrc
