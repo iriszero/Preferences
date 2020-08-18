faaa2dac288ee7be8f68be87f562034d7281630a
Jason Huh <jaeseok.huh.0@gmail.com>
2020-08-18 14:51:06 +0900

    Fix the Peter Parker Principle

--- linux/change_sudo_lecture/change_sudo_lecture.sh
+++ linux/change_sudo_lecture/change_sudo_lecture.sh
@@ -1,6 +1,9 @@
 # Show the Peter Parker Principle
-sudo mv sudoers.lecture /etc/
+
+BASE_DIR=$(dirname "$0")
+
+sudo mv $BASE_DIR/sudoers.lecture /etc/
 sudo chown root:root /etc/sudoers.lecture
-sudo mv privacy /etc/sudoers.d/
+sudo mv $BASE_DIR/privacy /etc/sudoers.d/
 sudo chown root:root /etc/sudoers.d/privacy
 
