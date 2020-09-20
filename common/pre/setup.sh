b06c2077a4a4fc2f563147393b53c9b5000ae76e
Jason Huh <jaeseok.huh.0@gmail.com>
2020-09-20 20:40:18 +0900

    Add yes to the apt command

--- common/pre/setup.sh
+++ common/pre/setup.sh
@@ -4,7 +4,7 @@ BASE_DIR=$(dirname "$0")
 
 # START git
 # Save your git crediental in memory (for 15 mins)
-sudo apt install git
+sudo apt install -y git
 git config --global credential.helper cache
 
 git config --global user.name "Jason Huh"
