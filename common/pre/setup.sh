0b107f91fb8cfd3212c900df0ac39b89ef77b287
Jason Huh <jaeseok.huh.0@gmail.com>
2020-10-16 05:53:29 +0900

    Rename .profile to .zprofile to support zsh

--- common/pre/setup.sh
+++ common/pre/setup.sh
@@ -10,5 +10,5 @@ git config --global credential.helper cache
 git config --global user.name "Jason Huh"
 git config --global user.email "jaeseok.huh.0@gmail.com"
 
-cat .profile >> $HOME/.profile
+cat .zprofile >> $HOME/.zprofile
 # END git
