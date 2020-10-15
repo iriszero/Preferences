40366301f77d2988a5a1eb1b3d3ec3fbcaddadb5
Jason Huh <jaeseok.huh.0@gmail.com>
2020-10-16 07:04:42 +0900

    Remove duplicate lines (see .gitconfig)

--- common/pre/setup.sh
+++ common/pre/setup.sh
@@ -5,10 +5,6 @@ BASE_DIR=$(dirname "$0")
 # START git
 # Save your git crediental in memory (for 15 mins)
 sudo apt install -y git
-git config --global credential.helper cache
-
-git config --global user.name "Jason Huh"
-git config --global user.email "jaeseok.huh.0@gmail.com"
 
 cat .zprofile >> $HOME/.zprofile
 # END git
