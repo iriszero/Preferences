977b17c110abb88291380989abc9e4f74aaa6a48
Jason Huh <jaeseok.huh.0@gmail.com>
2020-10-20 13:33:10 +0900

    Copy the files, rather than move

--- linux/change_sudo_lecture/change_sudo_lecture.sh
+++ linux/change_sudo_lecture/change_sudo_lecture.sh
@@ -2,8 +2,8 @@
 
 BASE_DIR=$(dirname "$0")
 
-sudo mv $BASE_DIR/sudoers.lecture /etc/
+sudo cp $BASE_DIR/sudoers.lecture /etc/
 sudo chown root:root /etc/sudoers.lecture
-sudo mv $BASE_DIR/privacy /etc/sudoers.d/
+sudo cp $BASE_DIR/privacy /etc/sudoers.d/
 sudo chown root:root /etc/sudoers.d/privacy
 
