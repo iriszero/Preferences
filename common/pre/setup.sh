d4bf67e72e3830951e48dc30ffb6bb41844bec51
Jason Huh <jaeseok.huh.0@gmail.com>
2020-10-20 12:15:06 +0900

    Add curl installation

--- common/pre/setup.sh
+++ common/pre/setup.sh
@@ -6,5 +6,8 @@ BASE_DIR=$(dirname "$0")
 # Save your git crediental in memory (for 15 mins)
 sudo apt install -y git
 
+# Curl
+sudo apt install -y curl
+
 cat .zprofile >> $HOME/.zprofile
 # END git
