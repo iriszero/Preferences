10b2b3872fb3ded8eecc2a634165477bd3a8799e
Jason Huh <jason.huh.now@gmail.com>
2021-02-20 10:44:52 +0900

    Add git-credential-manager

--- mac/setup.sh
+++ mac/setup.sh
@@ -6,7 +6,7 @@
 brew install cmake git htop vim zsh tmux
 brew install python python3 pyenv-virtualenv python-pip python3-pip
 brew install aria2
-
+brew install git-credential-manager
 brew install fasd
 
 # Powerline Fonts
