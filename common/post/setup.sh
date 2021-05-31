d9c846743143f82e68a66be741e1c090a733ed27
Jason Huh <jshuhnow@gmail.com>
2021-05-31 11:42:50 +0900

    Update setup.sh

--- common/post/setup.sh
+++ common/post/setup.sh
@@ -25,6 +25,8 @@ cat $BASE_DIR/.zshrc_color >> $HOME/.zshrc
 
 sudo chsh -s /usr/bin/zsh
 cat $BASE_DIR/.aliases >> $HOME/.aliases
+
+echo 'eval "$(fasd --init auto)"' >> $HOME/.zshrc
 echo "source ~/.aliases" >> $HOME/.zshrc
 
 # gitconfig
